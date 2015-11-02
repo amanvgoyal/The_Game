2be28684be664007c2b8b8ecd8cc24555b5f5d2d
Ronilo R <RoniloRagodos57@tamu.edu>
2015-11-02 16:29:34 -0600

    gui functionality mostly done

--- Client.java
+++ Client.java
@@ -1,3 +1,6 @@
+// Modified version of below
+// http://www.tutorialspoint.com/swing/swing_springlayout.htm
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -24,50 +27,171 @@ import java.net.Socket;
 import java.io.InputStreamReader;
 import java.net.UnknownHostException;
 
-public class Client {
-    public static void main(String s[]) throws IOException{
-	// Resources
-	File f = new File("logo.png");
-	BufferedImage img = ImageIO.read(f);
-	JLabel dispImg = new JLabel(new ImageIcon(img));
-	
-	final JFrame frame = new JFrame("b0ss Breakthrough client");
+public class SwingLayoutDemo {
+    private JFrame mainFrame;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+    private JLabel msglabel;
 
-	JPanel panel = new JPanel();
-	panel.setLayout(new FlowLayout());
+    private String AIdiff;
 
+    public SwingLayoutDemo(){
+	prepareGUI();
+    }
+
+    public static void main(String[] args) throws IOException{
+	SwingLayoutDemo swingLayoutDemo = new SwingLayoutDemo();  
+	swingLayoutDemo.showCardLayoutDemo();       
+    }
+      
+    private void prepareGUI(){
+	mainFrame = new JFrame("b0ss Breakthrough Client");
+	mainFrame.setSize(400,400);
+	mainFrame.setLayout(new GridLayout(3, 1));
+
+	statusLabel = new JLabel("",JLabel.CENTER);        
+
+	statusLabel.setSize(350,100);
+	mainFrame.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent windowEvent){
+		    System.exit(0);
+		}        
+	    });    
+	controlPanel = new JPanel();
+	controlPanel.setLayout(new FlowLayout());
+
+	mainFrame.add(controlPanel);
+	mainFrame.add(statusLabel);
+	mainFrame.setVisible(true);  
+    }
+   
+    private void showCardLayoutDemo(){
+	final JPanel panel = new JPanel();
+	Color maroon = new Color(128, 0, 0);
+	panel.setBackground(maroon);
+	panel.setSize(640, 480);
+
+	CardLayout layout = new CardLayout();
+	layout.setHgap(10);
+	layout.setVgap(10);
+	panel.setLayout(layout);        
+
+
+	// Screen for choosing game type
+	JPanel buttonPanel = new JPanel(new FlowLayout());
+
+	JButton gTypeBtn1 = new JButton("Human - AI");
+        gTypeBtn1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+		    JOptionPane.showMessageDialog(mainFrame,"Human-AI Chosen");
+		    CardLayout cardLayout = (CardLayout)(panel.getLayout());
+		    cardLayout.show(panel, "Command");
+		}
+	    });
+	    
+	JButton gTypeBtn2 = new JButton("AI - AI");
+        gTypeBtn2.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+		    JOptionPane.showMessageDialog(mainFrame,"AI-AI Chosen");
+		    CardLayout cardLayout = (CardLayout)(panel.getLayout());
+		    cardLayout.show(panel, "AIGame");
+		}
+            });
+
+	buttonPanel.add(gTypeBtn1);
+	buttonPanel.add(gTypeBtn2);
+
+	// Screen for entering password to log in 
+	JPanel passBoxPanel = new JPanel(new FlowLayout());
 	JLabel passLabel = new JLabel("Password: ");
-	final JPasswordField passField = new JPasswordField(10);
-	passLabel.setLabelFor(passField);
 
-	JButton login = new JButton("Login");
-        login.addActionListener(new ActionListener() {
+	final JPasswordField passField = new JPasswordField(10);
+	passBoxPanel.add(passLabel);
+	passBoxPanel.add(passField);
+
+	panel.add("Login", passBoxPanel);
+	panel.add("GameType", buttonPanel);
+
+
+	// Screen for entering commands in Human-AI game
+	JPanel cmdBoxPanel = new JPanel(new FlowLayout());
+	JLabel cmdLabel = new JLabel("Command: ");
+	final JTextField cmdField = new JTextField(20);
+	JButton cmdEnter = new JButton("Submit");
+	
+	cmdEnter.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-		    String password = new String(passField.getPassword());
-		    if (password.equals("password")) {
-			    JOptionPane.showMessageDialog(frame,"Welcome.");
-			    // Further logic...
-			    
-		    }
-		
-			else {
-			    JOptionPane.showMessageDialog(frame, "Try again.");
-			}
+		    String cmd = cmdField.getText();
+		    JOptionPane.showMessageDialog(mainFrame, cmd);
 		}
 	    });
 
+	cmdBoxPanel.add(cmdLabel);
+	cmdBoxPanel.add(cmdField);
+	cmdBoxPanel.add(cmdEnter);
 
-	panel.add(passLabel);
-	panel.add(passField);
-	panel.add(login);
-	panel.add(dispImg);
+	panel.add("Command", cmdBoxPanel);
 
-	frame.add(panel);
-	frame.setSize(640, 480);
-	frame.setLocationRelativeTo(null);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setVisible(true);
+	// Screen for choosing AI difficulty
+	JPanel AIDiffPanel = new JPanel(new FlowLayout());
+	JLabel diffLabel = new JLabel("Enter difficulty: ");
+	final JTextField diffField = new JTextField(15);
+	JButton diffEnter = new JButton("Submit");
+ 
+        diffEnter.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+		    String diff = diffField.getText();
+		    
+		    if (diff.equalsIgnoreCase("easy") ||
+			diff.equalsIgnoreCase("medium") ||
+			diff.equalsIgnoreCase("hard")) {
 
+			AIdiff = diff;
+			JOptionPane.showMessageDialog(mainFrame, diff);
+			CardLayout cardLayout = (CardLayout)(panel.getLayout());
+			cardLayout.show(panel, "GameType");
+		    }
+		    else {
+			JOptionPane.showMessageDialog(mainFrame, "Invalid!");
+		    }
+		}
+            });
+	
+	AIDiffPanel.add(diffLabel);
+	AIDiffPanel.add(diffField);
+	AIDiffPanel.add(diffEnter);
+	
+	panel.add("AIDiff", AIDiffPanel);
+
+
+	// Screen for AI-AI game
+	JPanel AIGamePanel = new JPanel(new FlowLayout());
+	JLabel AIGameLabel = new JLabel("AI-AI Game");
+	AIGamePanel.add(AIGameLabel);
+
+	panel.add("AIGame", AIGamePanel);
+
+	// Finish making control pannel by adding login button
+	final JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String password = new String(passField.getPassword());
+                    if (password.equals("password")) {
+			loginButton.setVisible(false);
+			JOptionPane.showMessageDialog(mainFrame,"Welcome.");
+			CardLayout cardLayout = (CardLayout)(panel.getLayout());
+			cardLayout.show(panel, "AIDiff");
+                    }
+
+		    else {
+			JOptionPane.showMessageDialog(mainFrame, "Try again.");
+		    }
+		}
+            });
+
+	controlPanel.add(loginButton);
+	controlPanel.add(panel);
+
+	mainFrame.setVisible(true);  
     }
-
 }
